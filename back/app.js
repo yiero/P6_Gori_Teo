9c3fc440db09493b588648acf18200c694205fed
yiero <goriteo1@hotmail.fr>
2022-03-25 01:22:57 +0100

    Création des schemas models

--- back/app.js
+++ back/app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require ('mongoose');
+
+mongoose.connect('mongodb+srv://Teo:passe@cluster0.y776q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
